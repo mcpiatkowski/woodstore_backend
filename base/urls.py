678e0c517d17056eff6ce2feb11e9d40c26fb538
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-11 17:50:32 +0200

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import getRoutes, getProducts, getProduct
+
+
+urlpatterns = [
+    path('', getRoutes, name='routes'),
+    path('products/', getProducts, name='products'),
+    path('products/<str:pk>/', getProduct, name='product'),
+]
