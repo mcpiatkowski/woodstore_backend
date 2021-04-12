d9c40b97bf76d0133f7c72fc455c2253cbc7410c
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-12 10:32:46 +0200

    serializers

--- base/admin.py
+++ base/admin.py
@@ -7,7 +7,11 @@ from .models import (
     ShippingAddress,
 )
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(OrderItem)
