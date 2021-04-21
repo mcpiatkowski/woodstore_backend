89e2f32230016fcc9dcd9aa405c1fd8ed734d2e6
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-21 08:40:38 +0200

    token customization

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import getRoutes, getProducts, getProduct
+from .views import MyTokenObtainPairView
 
 
 urlpatterns = [
+    path('users/login/', MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('', getRoutes, name='routes'),
     path('products/', getProducts, name='products'),
     path('products/<str:pk>/', getProduct, name='product'),
