de717b2d9af59f05bd757d1decdd2b148e982cad
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-04-21 10:34:15 +0200

    user serializer and permissions

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import getRoutes, getProducts, getProduct, getUserProfile
+from .views import getRoutes, getProducts, getProduct, getUserProfile, getUsers
 from .views import MyTokenObtainPairView
 
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('users/login/', MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
     path('users/profile/', getUserProfile, name='users-profile'),
+    path('users/', getUsers, name='users'),
     path('', getRoutes, name='routes'),
     path('products/', getProducts, name='products'),
     path('products/<str:pk>/', getProduct, name='product'),
