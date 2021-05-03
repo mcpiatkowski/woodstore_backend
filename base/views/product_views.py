a0279827f1481bcac932b8adfe96467ec4420a93
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-05-03 18:51:14 +0200

    pagination

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from base.models import Product, Review
 from base.serializers import ProductSerializer
@@ -13,8 +14,27 @@ from rest_framework import status
 @ api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
+    page = request.query_params.get('page')
+    paginator = Paginator(products, 5)
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    if page == None:
+        page = 1
+
+    page = int(page)
+
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    return Response({
+        'products': serializer.data,
+        'page': page,
+        'pages': paginator.num_pages
+    })
 
 
 @ api_view(['GET'])
@@ -87,11 +107,11 @@ def createProductReview(request, pk):
     alreadyExists = product.review_set.filter(user=user).exists()
 
     if alreadyExists:
-        content = {'details': 'Dodałeś już recenzję do tego produktu.'}
+        content = {'detail': 'Dodałeś już recenzję do tego produktu.'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
     elif data['rating'] == 0:
-        content = {'details': 'Dodaj ocenę.'}
+        content = {'detail': 'Dodaj ocenę.'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
 
     else:
